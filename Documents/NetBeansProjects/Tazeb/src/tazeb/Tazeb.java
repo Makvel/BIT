06426a2737d4e35e8bf2c726029e576852bf842a
Michale <Michale@Memey-PC>
2017-05-23 22:26:49 -0700

    our first program

---
+++ Documents/NetBeansProjects/Tazeb/src/tazeb/Tazeb.java
@@ -0,0 +1,14 @@
+
+package tazeb;
+
+import javax.swing.JOptionPane;
+
+
+public class Tazeb {
+
+   
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "HELLO WORLD");
+    }
+    
+}
