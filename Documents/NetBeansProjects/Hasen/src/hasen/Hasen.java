37597177f726c811287ed5e2e9545542121d6531
Michale <Michale@Memey-PC>
2017-05-24 13:47:58 -0700

    hasens project

---
+++ Documents/NetBeansProjects/Hasen/src/hasen/Hasen.java
@@ -0,0 +1,11 @@
+package hasen;
+
+import javax.swing.JOptionPane;
+
+public class Hasen {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hello world");
+    }
+    
+}
