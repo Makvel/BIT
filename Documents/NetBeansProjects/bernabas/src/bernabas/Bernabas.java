b78791f9165f7ec50377fb89cdc1ff0adee11f58
Michale <Michale@Memey-PC>
2017-05-24 13:49:56 -0700

    bernabases project

---
+++ Documents/NetBeansProjects/bernabas/src/bernabas/Bernabas.java
@@ -0,0 +1,13 @@
+
+package bernabas;
+
+import javax.swing.JOptionPane;
+
+
+public class Bernabas {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "ENDIET NEH ANTE FESAM");
+    }
+    
+}
