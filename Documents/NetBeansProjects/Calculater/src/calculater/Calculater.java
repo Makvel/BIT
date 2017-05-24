db3159285ce26e79fd47715a3f9df040c81c1724
Michale <Michale@Memey-PC>
2017-05-23 22:39:53 -0700

    our first program

---
+++ Documents/NetBeansProjects/Calculater/src/calculater/Calculater.java
@@ -0,0 +1,24 @@
+
+package calculater;
+
+
+public class Calculater {
+int a,b;
+
+public int add(){
+    int sum;
+    sum=a+b;
+    return sum;
+}   
+
+public int multiply(){
+    int pro;
+    pro=a*b;
+    return pro;
+    
+}
+    public static void main(String[] args) {
+       
+    }
+    
+}
